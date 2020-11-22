91c5fde53b483eb557d32b6126457e5ccc92badf
Oberdan Brito de Jesus <oberdanbdj@gmail.com>
2020-11-22 06:35:05 -0300

    Configura o objeto email para novo teste

--- src/teste.php
+++ src/teste.php
@@ -1,3 +1,20 @@
 <?php
 
-require 'Envio.php';
+use Craos\Email;
+
+require_once 'Email.php';
+$email = new Email\Email();
+
+$conta = (object)array(
+    'host' => 'smtp.office365.com',
+    'auth' => true,
+    'endereco' => 'correspondencia@animaclube.com.br',
+    'senha' => 'Sam74172',
+    'secure' => 'tls',
+    'port' => 587,
+    'ssl' => 'ca-cert.pem',
+    'remetente' => 'Condomínio Ânima Clube',
+    'dominio' => 'animaclube.com.br'
+);
+
+$email->setConta($conta);
