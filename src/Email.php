84d9cd6135b91a0fc35e262463857d76ee3b29f0
Oberdan Brito de Jesus <oberdanbdj@gmail.com>
2020-11-22 06:56:24 -0300

    Otimização de código

--- src/Email.php
+++ src/Email.php
@@ -30,53 +30,68 @@ class Email
 
     }
 
-    public function Iniciar() {
-
+    public function Configurar() {
+        $this->ConfigurarConta();
+        $this->ConfigurarCabecalho();
+        $this->ConfigurarSSL();
     }
 
     /**
      * @param mixed $conta
      */
-    public function setConta($conta): void
+    public function setParametrosCorrespondente($conta): void
     {
         $this->conta = $conta;
+        $this->Configurar();
+
+    }
+
+    private function ConfigurarConta()
+    {
+        $this->mail->Host = $this->conta->Smtp_host;
+        $this->mail->SMTPAuth = $this->conta->Smtp_auth;
+        $this->mail->Username = $this->conta->Endereco;
+        $this->mail->Password = $this->conta->Senha;
+        $this->mail->SMTPSecure = ($this->conta->Smtp_secure == 'TSL') ? PHPMailer::ENCRYPTION_STARTTLS : PHPMailer::ENCRYPTION_SMTPS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
+        $this->mail->Port = $this->conta->Smtp_port;
+    }
+
+    private function ConfigurarCabecalho()
+    {
 
         try {
-
-            $this->mail->Host = $this->conta->Smtp_host;
-            $this->mail->SMTPAuth = $this->conta->Smtp_auth;
-            $this->mail->Username = $this->conta->Endereco;
-            $this->mail->Password = $this->conta->Senha;
-            $this->mail->SMTPSecure = ($this->conta->Smtp_secure == 'TSL') ? PHPMailer::ENCRYPTION_STARTTLS : PHPMailer::ENCRYPTION_SMTPS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
-            $this->mail->Port = $this->conta->Smtp_port;
-            $this->mail->SMTPOptions = array(
-                'ssl' => array(
-                    'peer_name' => $this->conta->Smtp_host,
-                    'verify_peer' => true,
-                    'verify_peer_name' => true,
-                    'allow_self_signed' => true,
-                    'verify_depth' => 3,
-                    'cafile' => 'ca-cert.pem',
-                )
-            );
-
             $this->mail->setFrom($this->conta->Endereco, $this->conta->Nome);
             $this->mail->addReplyTo($this->conta->Endereco, $this->conta->Nome);
             $this->mail->AddCustomHeader( "X-Confirm-Reading-To: " . $this->conta->Endereco );
             $this->mail->AddCustomHeader( "Return-Receipt-To: " . $this->conta->Endereco );
-            $this->mail->AddCustomHeader( "Disposition-Notification-To: " . $this->conta->Endereco );
-
-            $this->mail->DKIM_domain = $this->conta->Dominio;
-            $this->mail->DKIM_private = 'api_dkim_private.pem';
-            $this->mail->DKIM_selector = 'api';
-            $this->mail->DKIM_passphrase = '';
-            $this->mail->DKIM_identity = $this->conta->Endereco;
-            $this->mail->DKIM_copyHeaderFields = false;
-            $this->mail->DKIM_extraHeaders = ['List-Unsubscribe', 'List-Help'];
-
+            $this->mail->AddCustomHeader("Disposition-Notification-To: " . $this->conta->Endereco);
         } catch (Exception $e) {
+            error_log($e->getMessage());
         }
-
-
     }
+
+    private function ConfigurarSSL()
+    {
+        $this->mail->SMTPOptions = array(
+            'ssl' => array(
+                'peer_name' => $this->conta->Smtp_host,
+                'verify_peer' => true,
+                'verify_peer_name' => true,
+                'allow_self_signed' => true,
+                'verify_depth' => 3,
+                'cafile' => 'ca-cert.pem',
+            )
+        );
+
+        $this->mail->DKIM_domain = $this->conta->Dominio;
+        $this->mail->DKIM_private = 'api_dkim_private.pem';
+        $this->mail->DKIM_selector = 'api';
+        $this->mail->DKIM_passphrase = '';
+        $this->mail->DKIM_identity = $this->conta->Endereco;
+        $this->mail->DKIM_copyHeaderFields = false;
+        $this->mail->DKIM_extraHeaders = ['List-Unsubscribe', 'List-Help'];
+    }
+
+
+
 }
